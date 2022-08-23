6991c00a197d8174ba15f4f123e32ef1951d5ac9
Millat Hossain <millathossain115@gmail.com>
2022-08-23 10:32:50 +0600

    Faculty can make comment on decription

---
+++ make_comment.php
@@ -0,0 +1,40 @@
+<html>
+	<head><title>Comment Create</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
+
+	</head>
+	<body><br></br>
+
+<form class="ui form" style="max-width: 700px; margin:0 auto;" action=comment_result.php>
+<h4 class="ui dividing header">Put your feedback about the project</h4>
+
+
+
+  <div class="field">
+    <!-- <label>Put your feedback about the project</label> -->
+    <input type=text name="FACULTY_FEEDBACK" placeholder="Make a comment">
+  </div>
+	
+  <div class="field">
+    <label>Group ID</label>
+    <input type=text name="GROUP_ID" placeholder="Integer No">
+  </div>
+
+
+
+	
+	<button class="ui button" type="submit">Submit</button>
+</form>
+</body>
+</html>
+
+<!-- PROJECT_ID
+PROJECT_TITLE
+GROUP_ID
+MARKS
+ROOM_NO
+PROJECT_DESCRIPTION
+CATEGORY
+FACULTY_ID -->
+<!-- FACULTY_FEEDBACK -->
+
