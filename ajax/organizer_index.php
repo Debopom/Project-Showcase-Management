6e73c94ffa8b2a48439c769be3e73a9091b04e59
Millat Hossain <millathossain115@gmail.com>
2022-09-19 04:34:11 +0600

    organizer landing page added

---
+++ ajax/organizer_index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index Page</title>
+    <link rel="stylesheet" href="organizer_style.css">
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+    <!-- MDB -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/5.0.0/mdb.min.css" rel="stylesheet" />
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+   
+</head>
+<body>
+
+    <div class="container">
+        <h1>Add Organizer</h1>
+        
+        <input id="name"type="text" placeholder="Enter Name...."required> <br><br>
+        <!-- <input id="s_id"type="text" placeholder="Student ID...." required> <br><br> -->
+        <input id="email"type="email" placeholder="Enter Email...."required> <br><br>
+        <input id="phone"type="text" placeholder="Enter Phone Number...."required> <br><br>
+        <!-- <input id="email"type="email" placeholder="Enter Email...."required> <br><br> -->
+        <!-- <input type="Text" placeholder="Enter department...."> <br><br> -->
+
+
+        <label for="role">Select your Role </label><br>
+        <select id="role" name="role">
+            <option value="CSE">Organizer</option>
+            <option value="EEE">Judge</option>
+            <option value="BBA">Faculty</option>
+            <!-- <option value="CIVIL">CIVIL</option> -->
+        </select><br><br>
+
+        <label for="dept">Select your Department </label><br>
+        <select id="dept" name="dept">
+            <option value="CSE">CSE</option>
+            <option value="EEE">EEE</option>
+            <option value="BBA">BBA</option>
+            <option value="CIVIL">CIVIL</option>
+            <option value="Textile">Textile</option>
+            <option value="Economics">Economics</option>
+        </select><br><br>
+
+        <button class="button">Save</button><br>
+        <!-- <button id="btn2">Save</button> -->
+                           
+    </div>
+    
+
+
+
+
+
+
+<!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="organizer_ajax.js"></script>
+</body>
+</html>
